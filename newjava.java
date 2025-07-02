2ded590c76def2532a5e727d4cdecd6633301f04
sgaduputi18 <saikrishnagaduputi1118@gmail.com>
2025-07-02 16:24:25 -0500

    Update newjava.java

--- newjava.java
+++ newjava.java
@@ -1,5 +1,13 @@
-public newjava Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+public class newjava {
+    public static void main(String[] args) {
+        // Loop through numbers 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            // Check if the number is even or odd
+            if (i % 2 == 0) {
+                System.out.println(i + " is Even");
+            } else {
+                System.out.println(i + " is Odd");
+            }
+        }
+    }
 }
