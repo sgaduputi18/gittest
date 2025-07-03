1063d12218d2bf7e61a5926c1bd42a9a6d05721b
sgaduputi18 <saikrishnagaduputi1118@gmail.com>
2025-07-03 12:38:39 -0500

    Update newjava.java

--- newjava.java
+++ newjava.java
@@ -4,7 +4,7 @@ public class newjava {
         for (int i = 1; i <= 10; i++) {
             // Check if the number is even or odd
             if (i % 2 == 0) {
-                System.out.println(i + " is Even");
+                System.out.println(i + " is Evebhkjkbkkjkkjbbn");
             } else {
                 System.out.println(i + " is Odd");
             }
